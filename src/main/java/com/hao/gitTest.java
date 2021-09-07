bc737d13d0b500535e5c6263c616549c61b6d4b2
Afy <954543426@qq.com>
2021-09-07 21:33:55 +0800

    push commit

--- src/main/java/com/hao/gitTest.java
+++ src/main/java/com/hao/gitTest.java
@@ -10,5 +10,6 @@ public class gitTest {
     public static void main(String[] args) {
         System.out.println("你好，git333！");
         System.out.println("你好，git4！");
+        System.out.println("你好，pushnew！");
     }
 }
