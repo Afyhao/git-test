288c741685e6bd90dbef30c73a3d694d716383e4
Afy <954543426@qq.com>
2021-09-07 20:51:00 +0800

    hot-fix commit1

--- src/main/java/com/hao/gitTest.java
+++ src/main/java/com/hao/gitTest.java
@@ -8,6 +8,6 @@ import javax.activation.MailcapCommandMap;
  */
 public class gitTest {
     public static void main(String[] args) {
-        System.out.println("你好，git！");
+        System.out.println("你好，git4！");
     }
 }
