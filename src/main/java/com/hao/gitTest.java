b133b4a53e678e971aced3420d79150ee2559280
Afy <954543426@qq.com>
2021-09-07 19:50:17 +0800

    first commit

---
+++ src/main/java/com/hao/gitTest.java
@@ -0,0 +1,13 @@
+package com.hao;
+
+import javax.activation.MailcapCommandMap;
+
+/**
+ * @author shkstart
+ * @create 2021-09 19:47
+ */
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("你好，git！");
+    }
+}
